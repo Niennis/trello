b93b319f0da858fe0405f34933621b484650acb1
Niennis <estefania.osses.v@gmail>
2017-11-08 13:37:09 -0300

    segundo avance

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 window.onload = function(){
 
+    /* LLAMANDO ELEMENTOS */
     var addList = document.getElementById('lista'); /*Texto que desaparece*/
     var form = document.getElementById('form');/* Form que reemplaza al texto */
 
@@ -7,30 +8,36 @@ window.onload = function(){
     var guardar = document.getElementById('guardar'); /* Boton Guardar */
     var cancel = document.getElementsByClassName('cancel')[0]; /* Boton Cancelar */
     var newList = document.getElementById('getList'); /* input = nombre de lista */
-    
     var container = document.getElementById('lists'); 
 
+
+    /* CREANDO ELEMENTOS */
     var listDiv = document.createElement('div'); /* Div de lista hecha */
    /* var formList = document.createElement('form');*/
-    var listName = document.createElement('p');
-    var addCard = document.createElement('p');
-    addCard.value = 'Añadir una tarjeta...'
+    var listName = document.createElement('p'); /* Nombre de la lista */
+    var addCard = document.createElement('p'); /* Añadir una tarjeta... */
+    addCard.textContent = 'Añadir una tarjeta...'
+
 
 /* FUNCION PARA CAMBIAR DE TEXTO A LISTA */
     addList.onclick = function(){
         addList.style.display='none';
         form.style.display='inline-block';
     }
-
+    
+/* FUNCIÓN PARA AGREGAR NOMBRE DE LISTA Y DESPLEGAR MENU DE TARJETAS */
     guardar.onclick = function(){
         if (newList.value == ''){
-            /*guardar.disabled = true;*/
+          /*  guardar.disabled = true;*/
+
         }else{
             /*guardar.disabled = false;*/
+
             listName.textContent = newList.value;
             listDiv.appendChild(listName);
             listDiv.appendChild(addCard);
             container.appendChild(listDiv);
+            listDiv.id='boxList';
         }
     }
 
